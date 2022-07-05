e7a6dc5925aadb57d3409655bdd5878d63d1a1db
Byron Houwens <zhion360@gmail.com>
2022-07-05 13:24:27 +0200

    Removing front end

--- utils.js
+++ utils.js
@@ -1,3 +1,11 @@
+/**
+ * Generic constructor for consistent response handling
+ * 
+ * @param {number} status - HTTP status code
+ * @param {string} message - Message about status
+ * @param {Object} data - Data to be returned
+ * @returns 
+ */
 function constructResponse(status, message, data) {
     return {
         status: status,
@@ -6,11 +14,24 @@ function constructResponse(status, message, data) {
     };
 }
 
+/**
+ * Constructs an error-specific response
+ * 
+ * @param {number} status - HTTP status code
+ * @param {string} message - Error message
+ * @returns 
+ */
 function errorResponse(status, message) {
     return constructResponse(status, message, null);
 }
 
+/**
+ * Gets all keypairs stored on disk
+ * 
+ * @returns 
+ */
 function getAllKeypairs() {
+    console.error("getAllKeypairs not yet implemented!");
     return [];
 }
 
